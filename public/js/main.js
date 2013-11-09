95bf21c5e8c2a4c8badb3b6af166aad9f3c36e02
Joe Andaverde <joe@andaverde.net>
2013-11-08 22:03:08 -0600

    Added logging

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,11 @@
 window.game = { session: null, publisher: null };
 
 (function (game) {
-    var log = function (message) {
-        console.log(message);
+    var log = function () {
+        var now = new Date();
+        now = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + "." + now.getMilliseconds();
+        arguments = [now].concat(arguments);
+        console.log.apply(console, arguments);
     };
 
     $(document).ready(function () {
@@ -17,16 +20,20 @@ window.game = { session: null, publisher: null };
             var stream = streams[i];
             if (stream.connection.connectionId != game.session.connection.connectionId) {
                 game.session.subscribe(stream, get_element_id_for_stream(stream));
+                log('subscribing to stream ', stream);
+
             }
         }
     }
 
     function on_session_connected (event) {
+        log('session created', event);
         game.session.publish(game.publisher);
         subscribe_to_streams(event.streams);
     }
 
     function on_stream_created(event) {
+        log('stream created', event);
         subscribe_to_streams(event.streams);
     }
 
@@ -38,13 +45,13 @@ window.game = { session: null, publisher: null };
     };
 
     var start = function () {
+        log('start');
         join_room();
-        end();
     };
 
     var end = function () {
-        game.session.removeEventListener('sessionConnected');
-        game.session.removeEventListener('streamCreated');
+        game.session.off('sessionConnected');
+        game.session.off('streamCreated');
         game.session.disconnect();
         game = {};
     };
@@ -54,6 +61,9 @@ window.game = { session: null, publisher: null };
         var session_id = room_id || "1_MX40NDM5MzQ3Mn5-RnJpIE5vdiAwOCAxODoyMzo1MyBQU1QgMjAxM34wLjU2OTE2NzF-";
         var token = "T1==cGFydG5lcl9pZD00NDM5MzQ3MiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz00MDYzNzFjMTM4NDY1MmJmYWRmOTVlZmNkOWVlZTk3YTc2ZDQzNzk1OnJvbGU9cHVibGlzaGVyJnNlc3Npb25faWQ9MV9NWDQwTkRNNU16UTNNbjUtUm5KcElFNXZkaUF3T0NBeE9Eb3lNem8xTXlCUVUxUWdNakF4TTM0d0xqVTJPVEUyTnpGLSZjcmVhdGVfdGltZT0xMzgzOTYzODU5Jm5vbmNlPTAuMjI5NDU1MzkxNTM0NTUxMiZleHBpcmVfdGltZT0xMzg0MDUwMjU5JmNvbm5lY3Rpb25fZGF0YT0=";
 
+        log('Joining session ' + session_id);
+
+        TB.setLogLevel(TB.NONE);
         game.publisher = TB.initPublisher(api_key);
         game.session = TB.initSession(session_id);
         game.session.connect(api_key, token);

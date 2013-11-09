17ebe4da84b905ff3c33336b3de0b8e7818bc9a8
joelongstreet <joelongstreet@gmail.com>
2013-11-09 11:16:17 -0600

    clean up

--- public/js/controllers/game-controller.js
+++ public/js/controllers/game-controller.js
@@ -15,7 +15,6 @@ define([
     play : function(params){
 
       // Handle the opentok session connection stuff
-      //TB.setLogLevel(TB.NONE);
       var self    = this;
       var api_key = '44393472';
       this.game   = {
@@ -23,34 +22,22 @@ define([
       }
       this.game.session.connect(api_key, params.token);
 
-      self.game.session.on('sessionConnected', on_session_connected);
-      self.game.session.on('streamCreated', on_stream_created);
-
-      // TODO - clean all this shit up a bit
-      var log = function () {
-        console.log.apply(console, arguments);
-      };
-
-      function on_session_connected (event) {
-        log('session created', event);
+      self.game.session.on('sessionConnected', function(e){
         self.game.session.publish(self.game.publisher);
-        subscribe_to_streams(event.streams);
-      }
+        subscribeToStreams(e.streams);
+      });
 
-      function on_stream_created(event) {
-        log('stream created', event);
-        subscribe_to_streams(event.streams);
-      }
+      self.game.session.on('streamCreated', function(e){
+        subscribeToStreams(e.streams);
+      });
 
-      function subscribe_to_streams(streams) {
-        if (!self.game.session)
-            return;
+      var subscribeToStreams = function(streams) {
+        if (!self.game.session) return;
 
         for (var i = 0; i < streams.length; i++) {
             var stream = streams[i];
             if (stream.connection.connectionId != self.game.session.connection.connectionId) {
                 self.game.session.subscribe(stream, get_element_id_for_stream(stream));
-                log('subscribing to stream ', stream);
             }
         }
       }

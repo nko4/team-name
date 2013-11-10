4d851d8340389ebe25308b855a9b9631de43b3a1
joelongstreet <joelongstreet@gmail.com>
2013-11-10 09:50:00 -0600

    more minor ui tweaks

--- public/js/controllers/base/controller.js
+++ public/js/controllers/base/controller.js
@@ -8,6 +8,11 @@ define([
   var Controller = Chaplin.Controller.extend({
     beforeAction : function(){
       Chaplin.Controller.prototype.beforeAction.apply(this, arguments);
+
+      // Hide the header if playing
+      if(arguments[1].action == 'play') $('#site-navigation').hide();
+      else $('#site-navigation').show()
+
       this.compose('site', SiteView);
     }
   });
